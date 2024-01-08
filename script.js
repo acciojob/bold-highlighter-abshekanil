79ff415871259eb2dd274976b37fc3d6014e95b2
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-08 19:31:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
+	let boldWords = document.querySelectorAll('p strong');
+	boldWords.forEach(function(word){
+		word.style.color = 'green';
+	});
 
 }
 
 
 function return_normal() {
     //Write your code here
+	let boldWords = document.querySelectorAll('p strong');
+	boldWords.forEach(function(word){
+		word.style.color = 'black';
+	});
 
     
 }
